3842412e3bc6a8b1f0cd595f24f813775ad55a9d
Yacine Hamza-Cherif <yassinehm17@gmail.com>
2017-03-02 00:29:11 +0100

    mise a jour d affichage des articles a la page d accueil et supression des boutons fb

--- js/myScript.js
+++ js/myScript.js
@@ -7,14 +7,18 @@ $('.wrapper-container .login-popup .fa-stack').on('click', function () {
 $('.se-connecter').on('click', function () {
     $(".wrapper-container").show();
 });
-
+/**
+ * Afficher la page Se connecter
+ */
 $('.wrapper-container .login-popup .sign-up a').on('click', function () {
 
     $('.sign-up').slideUp(400, function () {
         $('.sign-in').slideDown();
     });
 })
-
+/**
+ * Afficher la page Creer
+ */
 $('.wrapper-container .login-popup .sign-in a').on('click', function () {
 
 
@@ -22,13 +26,11 @@ $('.wrapper-container .login-popup .sign-in a').on('click', function () {
         $('.sign-up').slideDown();
     });
 })
-
+/**
+ * Afficher le mot de passe lorsque l'utilisateur passe le pointeur sur l'oeil
+ */
 $('.show-pass').hover(function () {
     $(this).parent().children().first().attr('type', 'text');
     },function () {
     $(this).parent().children().first().attr('type', 'password');
 })
-/*
-$('.product').hover(function () {
-    $()
-})*/
