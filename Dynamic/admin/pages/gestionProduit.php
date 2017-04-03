08a1bfeca19000237db702dcfe5eb5428a0423f9
Yacine Hamza-Cherif <yassinehm17@gmail.com>
2017-04-03 18:55:26 +0100

    Gestion des prduit

---
+++ Dynamic/admin/pages/gestionProduit.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yacine
+ * Date: 03/04/2017
+ * Time: 18:24
+ */?>
+<div class="gestionProduit">
+    <div class="container">
+        <h1 class="text-center">Gestion des produits</h1>
+        <form>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Rechercher un produit">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                </span>
+            </div>
+        </form>
+        <table class="table">
+            <tr>
+                <th>Reference</th>
+                <th colspan="2">Produit</th>
+                <th>Categorie</th>
+                <th>Prix</th>
+                <th>Etat de vente</th>
+                <th>Réduction</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td><img src="pages/im.png" width="50" height="30"></td>
+                <td>Prdouit 1</td>
+                <td>Informatique</td>
+                <td>500.00 DA</td>
+                <td>Sans promotion</td>
+                <td>0%</td>
+                <td><button class="btn btn-success">Modifier</button></td>
+                <td><button class="btn btn-danger">Supprimer</button></td>
+            </tr>
+        </table>
+    </div>
+</div>
