356fde77860103a23c952c18e3dcf1e1c954c56b
FAYSAL <abstrait1995@hotmail.fr>
2017-04-11 11:32:56 +0100

    "l'employe ne se conecte pas a del index il se connecte de la page principale uniquement comme pour l'admin maquette a respecter "

--- Dynamic/admin/pages/login.php
+++ Dynamic/admin/pages/login.php
@@ -5,6 +5,7 @@
  * Date: 03/04/2017
  * Time: 22:58
  */
+
 ?>
 <div class="container">
     <div class="login col-md-4 col-md-offset-4">
@@ -21,7 +22,7 @@
                     <input type="password" class="form-control" id="password" name="motDePasse" placeholder="Mot de passe" required="required" autocomplete="new-password">
                 </div>
             </div>
-            <input type="submit" class="btn btn-primary btn-block">
+            <input type="submit" class="btn btn-primary btn-block" >
         </form>
     </div>
 </div>
